090d4858d1224e3c24dac73b1d97f586457463ce
Charath123 <charathkumar098@gmail.com>
2024-09-22 15:06:17 +0530

    backend

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,55 +1,84 @@
 import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Login from "./Login";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
 function Signup() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/signup", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Signup Successfully");
+          navigate(from, { replace: true });
+        }
+        localStorage.setItem("Users", JSON.stringify(res.data.user));
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+        }
+      });
+  };
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <div className=" w-[600px] ">
           <div className="modal-box">
-            <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
               >
                 ✕
-              </a>
+              </Link>
 
-              <h3 className="font-bold text-lg">Sign Up</h3>
+              <h3 className="font-bold text-lg">Signup</h3>
               <div className="mt-4 space-y-2">
                 <span>Name</span>
-                <br></br>
+                <br />
                 <input
                   type="text"
-                  placeholder="Enter your Name"
+                  placeholder="Enter your fullname"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
-                  {...register("name", { required: true })}
+                  {...register("fullname", { required: true })}
                 />
-                <br></br>
-                {errors.name && (
+                <br />
+                {errors.fullname && (
                   <span className="text-sm text-red-500">
                     This field is required
                   </span>
                 )}
               </div>
+              {/* Email */}
               <div className="mt-4 space-y-2">
                 <span>Email</span>
-                <br></br>
+                <br />
                 <input
                   type="email"
-                  placeholder="Enter your Email"
+                  placeholder="Enter your email"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
                   {...register("email", { required: true })}
                 />
-                <br></br>
+                <br />
                 {errors.email && (
                   <span className="text-sm text-red-500">
                     This field is required
@@ -59,14 +88,14 @@ function Signup() {
               {/* Password */}
               <div className="mt-4 space-y-2">
                 <span>Password</span>
-                <br></br>
+                <br />
                 <input
                   type="password"
                   placeholder="Enter your password"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
                   {...register("password", { required: true })}
                 />
-                <br></br>
+                <br />
                 {errors.password && (
                   <span className="text-sm text-red-500">
                     This field is required
@@ -76,26 +105,19 @@ function Signup() {
               {/* Button */}
               <div className="flex justify-around mt-4">
                 <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-                  Login
+                  Signup
                 </button>
-                <p>
-                  Not registered?{" "}
-                  {/* <Link
-                  to="/"
-                  className="underline text-blue-500 cursor-pointer "
-                >
-                  SignUp
-                </Link> */}
-                  <a
-                    // href="/"
-                    className="text-blue-500"
+                <p className="text-xl">
+                  Have account?{" "}
+                  <button
+                    className="underline text-blue-500 cursor-pointer"
                     onClick={() =>
                       document.getElementById("my_modal_3").showModal()
                     }
                   >
                     Login
-                  </a>
-                  <Login></Login>
+                  </button>{" "}
+                  <Login />
                 </p>
               </div>
             </form>
